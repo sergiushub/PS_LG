e3ddde69e1db3819e266ff76b6d95af8c489a60e
sergiushub <sergio.dmgz@gmail.com>
2018-08-03 20:37:33 +1000

    Module structure change and stress test

--- Ejercicio1/classifynum.py
+++ Ejercicio1/classifynum.py
@@ -1,97 +1,94 @@
 """
-MyClassifier class with the solution of the code test for DEVO
+Module classifynum with the solution of the code test for DEVO
 """
 
-class MyClassifier:
+def classifylist(listanums):
+	"""
+	Classifylist recieves a list of elements and classify each 
+	one using classifynum function.
 
-	def classifylist(self,listanums):
-		"""
-		Classifylist recieves a list of elements and classify each 
-		one using classifynum function.
+	Args:
+		listanums (list): A list with the elements to classify
 
-		Args:
-			listanums (list): A list with the elements to classify
+	Returns:
+		Nothing
+	""" 
 
-		Returns:
-			Nothing
-		""" 
+	#We iterate the list to get the classification
+	for num in listanums:
+		tipo=classifynum(num)
+		print num,tipo
 
-		#We iterate the list to get the classification
-		for num in listanums:
-			tipo=self.classifynum(num)
-			print num,tipo
+def classifynum(number):
+	"""
+	Classifynum recieves an element and checks weather the element
+	is an integer number. If the element is a string representation
+	of an integer number the function parse the string to integer 
+	and classifies it. If the element is not a integer number or a 
+	string representation of an integer number the function returns 
+	the statement "no es un entero".
 
-	def classifynum(self,number):
-		"""
-		Classifynum recieves an element and checks weather the element
-		is an integer number. If the element is a string representation
-		of an integer number the function parse the string to integer 
-		and classifies it. If the element is not a integer number or a 
-		string representation of an integer number the function returns 
-		the statement "no es un entero".
+	There are three possible classifications:
+		"perfecto"
+		"abundante"
+		"defectivo" 
 
-		There are three possible classifications:
-			"perfecto"
-			"abundante"
-			"defectivo" 
+	Args:
+		number (object): An element which could be a number.
 
-		Args:
-			number (object): An element which could be a number.
-
-		Returns:
-			str: the name of the classification or the statement "no es un entero".
-			
-		"""
-
-		acc = 0
-
-		#First we check if the parameter recieved is in fact a integer
-		if isinstance(number, basestring):
-			if (self.is_integer(number)):
-				number = int(number)
-			else:
-				return "no es un entero"
-		elif not isinstance(number, int):
-			return "no es un entero"
-
-		#We itearate through each number from 1 to the total divided by two
-		#because there are no more divisors after half of the total
-		for i in range(1,number/2+1):
-			#If the number is a divisor we add to the total
-			if (number%i==0):
-				acc+=i
-
-		#Lastly we classify the number
-		if acc == number:
-			return "perfecto"
-		elif acc > number:
-			return "abundante"
-		else:
-			return "defectivo"
-
-	def is_integer(self,s):
-		"""
-		is_integer recieves a string and resolves wheather the string represents
-		an integer or not.
-
-		Args:
-			s (str): A string which could be a integer.
-
-		Returns:
-			bool: True if the string is an integer False if not.
-			
-		"""
+	Returns:
+		str: the name of the classification or the statement "no es un entero".
 		
-		#If the integer parsing succeds then the string is a representation
-		#of an integer and we return True. False otherwise.
-		try:
-			int(s)
-			return True
-		except ValueError:
-			return False
+	"""
 
+	acc = 0
 
-classifier = MyClassifier()
+	#First we check if the parameter recieved is in fact a integer
+	if isinstance(number, basestring):
+		if (is_integer(number)):
+			number = int(number)
+		else:
+			return "no es un entero"
+	elif not isinstance(number, int):
+		return "no es un entero"
 
-my_test_list = (1, 2, 12, 14, "15.1", "28", 15.9, "DEVO")
-classifier.classifylist(my_test_list)
+	#We itearate through each number from 1 to the total divided by two
+	#because there are no more divisors after half of the total
+	for i in range(1,number/2+1):
+		#If the number is a divisor we add to the total
+		if (number%i==0):
+			acc+=i
+
+	#Lastly we classify the number
+	if acc == number:
+		return "perfecto"
+	elif acc > number:
+		return "abundante"
+	else:
+		return "defectivo"
+
+def is_integer(s):
+	"""
+	is_integer recieves a string and resolves wheather the string represents
+	an integer or not.
+
+	Args:
+		s (str): A string which could be a integer.
+
+	Returns:
+		bool: True if the string is an integer False if not.
+		
+	"""
+	
+	#If the integer parsing succeds then the string is a representation
+	#of an integer and we return True. False otherwise.
+	try:
+		int(s)
+		return True
+	except ValueError:
+		return False
+
+import random
+
+my_random_list = [random.choice(range(45000,50000)) for i in range(10000)]
+classifylist(my_random_list)
